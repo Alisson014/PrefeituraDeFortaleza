9aea7b0c99a342785862b06b9d7a0978d50c759a
José Alisson <josealissondiasdacostaf10@gmail.com>
2025-01-05 15:51:36 -0300

    Carrossel de Comentários

--- js/script.js
+++ js/script.js
@@ -200,3 +200,75 @@ function ShowMenuFooter(){
         MenuFooter.classList.remove("AparecerMenu");
     }
 }
+
+
+const ConteinerComentarios = document.querySelector(".ConteinerComentarios");
+const comentarios = ConteinerComentarios.children;
+var indice = 0;
+var intervalo = 6000;
+var StartComentarios = false;
+
+
+const isVisible = (element) => {
+    const rect = element.getBoundingClientRect()
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+}
+
+function ShowComentarios(){
+    indice ++;
+    
+    if (indice >= comentarios.length){
+        indice = 0;
+    }
+
+    comentarios[indice].scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center"
+    });
+}
+function Loop1(){
+    if (isVisible(ConteinerComentarios)){
+        ShowComentarios();
+    }
+    console.log("teste");
+    setTimeout(() => {Loop2();}, intervalo);
+}
+function Loop2(){
+    if (isVisible(ConteinerComentarios)){
+        ShowComentarios();
+    }
+    console.log("teste");
+    setTimeout(() => {Loop1();}, intervalo);
+}
+
+Loop1();
+
+
+
+
+
+
+// const observer = new IntersectionObserver(entries => {
+    
+    
+//     Array.from(entries).forEach(entry => {
+//         if (entry.isIntersecting){
+//             StartComentarios = true;
+//             Comecar();
+//         }
+//         else{
+//             StartComentarios = false;
+//         }
+//     });
+// });
+// // , {
+// //     threshold: 0.7
+// // }
+
+// observer.observe(document.querySelector(".ConteinerComentarios"));
